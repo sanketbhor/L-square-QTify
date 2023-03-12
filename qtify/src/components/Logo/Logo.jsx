010610c3fdf5872b2a9f856f1d6b98d3b3b8686b
Sammy <sanketbhor535@gmail.com>
2023-03-13 01:17:45 +0530

    Feedback Button added

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import Logoimage from "../../assests/logo.png";
 
 export default function Logo() {
-  return <img src={Logoimage} alt="Qtify" width={"67px"} />;
+  return <img src={Logoimage} alt="Qtify" width={"67px"} height={"34px"} />;
 }
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "../Logo/Logo";
+import Feedback from "../Button/Feedback";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
       <Logo />
+      <Feedback />
     </div>
   );
 }
