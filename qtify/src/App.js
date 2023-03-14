6bcfb5f61e67ab550a83425cc1c5c14e24ed2497
Sammy <sanketbhor535@gmail.com>
2023-03-15 00:30:53 +0530

    Test commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Heroimage from "./components/Heroimage/Heroimage";
 import Card from "./components/Card/Card";
+import Section from "./components/Section/Section";
 
 function App() {
   return (
     <>
       <Navbar />
       <Heroimage />
-      <Card />
+      <Section title="Top Albums" />
     </>
   );
 }
---
+++ qtify/src/components/Section/Section.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Section(title) {
+  return <div>{title}</div>;
+}
+
+export default Section;
